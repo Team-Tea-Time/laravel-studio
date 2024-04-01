90afcf885bbc3a18d84e6e595919b23b4a55e3b0
Rick Mann <3583774+Riari@users.noreply.github.com>
2024-04-01 22:28:25 +0100

    Fix Xdebug

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -17,8 +17,4 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })
-    ->booting(function (Application $app) {
-        if (!$app->runningInConsole()) {
-            putenv('XDEBUG_SESSION=laravel');
-        }
-    })->create();
+    ->create();
