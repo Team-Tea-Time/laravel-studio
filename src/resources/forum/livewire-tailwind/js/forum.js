df4c6dc0b26b57e7667da54b1b88301fba32f969
Rick Mann <3583774+Riari@users.noreply.github.com>
2024-04-01 22:28:39 +0100

    Include Echo in livewire-tailwind preset JS

--- src/resources/forum/livewire-tailwind/js/forum.js
+++ src/resources/forum/livewire-tailwind/js/forum.js
@@ -3,6 +3,8 @@ import '@melloware/coloris/dist/coloris.css';
 import Coloris from '@melloware/coloris';
 import NestedSort from 'nested-sort';
 
+import '../../../js/echo';
+
 window.dateFormatDistance = formatDistance;
 window.Coloris = Coloris;
 window.Coloris.init();
